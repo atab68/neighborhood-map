53acce8774a192a1eaac2522be59d3e2bc07a660
atab68 <atabaldieva86@gmail.com>
2018-11-14 17:39:05 -0600

    Updates

---
+++ precache-manifest.ce965cb84515a465d76354f5efacf253.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1627007f1dec8dd542c",
+    "url": "/neighborhood-map/static/js/runtime~main.f1627007.js"
+  },
+  {
+    "revision": "7aa71f8d5844e36b04ef",
+    "url": "/neighborhood-map/static/js/main.7aa71f8d.chunk.js"
+  },
+  {
+    "revision": "dfed476836946b539c01",
+    "url": "/neighborhood-map/static/js/1.dfed4768.chunk.js"
+  },
+  {
+    "revision": "7aa71f8d5844e36b04ef",
+    "url": "/neighborhood-map/static/css/main.0be9e733.chunk.css"
+  },
+  {
+    "revision": "592e5c75cce4d8e0a20693f5795acf88",
+    "url": "/neighborhood-map/index.html"
+  }
+];
